6fbd73327d8a1313238571d68dd2c6a5f77587e4
Ade-onojobi Olugbayike <adeonojobiolugbayike@yahoo.com>
2024-02-10 13:52:13 +0100

    Started work on recycler view, view adapter and view holder

--- CriminalIntent/app/build.gradle.kts
+++ CriminalIntent/app/build.gradle.kts
@@ -45,6 +45,8 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.fragment:fragment-ktx:1.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
     testImplementation("junit:junit:4.13.2")
     testImplementation("junit:junit:4.12")
     debugImplementation("androidx.fragment:fragment-testing:1.6.2")
---
+++ CriminalIntent/app/src/main/java/com/olugbayike/android/criminalintent/CrimeListViewModel.kt
@@ -0,0 +1,21 @@
+package com.olugbayike.android.criminalintent
+
+import androidx.lifecycle.ViewModel
+import java.util.Date
+import java.util.UUID
+
+class CrimeListViewModel: ViewModel() {
+    val crimes = mutableListOf<Crime>()
+
+    init {
+        for(i in 0 until 100) {
+            val crime = Crime(
+                id = UUID.randomUUID(),
+                title = "crime #$i",
+                date = Date(),
+                isSolved = i % 2 == 0
+            )
+            crimes += crime
+        }
+    }
+}
