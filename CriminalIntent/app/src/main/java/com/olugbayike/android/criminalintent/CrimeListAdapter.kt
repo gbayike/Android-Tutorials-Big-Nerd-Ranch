d0f64a9f59db62d80d692ca79d8d0c96263e5601
Ade-onojobi Olugbayike <adeonojobiolugbayike@yahoo.com>
2024-02-10 21:38:39 +0100

    added constraint layout, added the vector for serious crimes, and cleaned up the UI

--- CriminalIntent/app/src/main/java/com/olugbayike/android/criminalintent/CrimeListAdapter.kt
+++ CriminalIntent/app/src/main/java/com/olugbayike/android/criminalintent/CrimeListAdapter.kt
@@ -1,6 +1,7 @@
 package com.olugbayike.android.criminalintent
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
@@ -8,14 +9,15 @@ import androidx.viewbinding.ViewBinding
 import com.google.android.material.snackbar.Snackbar
 import com.olugbayike.android.criminalintent.databinding.ListItemCrimeBinding
 import com.olugbayike.android.criminalintent.databinding.ListItemSeriousCrimeBinding
+import java.text.DateFormat
 
 class CrimeHolder(
-    private val binding: ViewBinding
+    private val binding: ListItemCrimeBinding
 ): RecyclerView.ViewHolder(binding.root){
     fun bind(crime: Crime){
-        if(binding is ListItemCrimeBinding){
+//        if(binding is ListItemCrimeBinding){
             binding.crimeTitle.text = crime.title
-            binding.crimeDate.text = crime.date.toString()
+            binding.crimeDate.text = DateFormat.getDateInstance(DateFormat.FULL).format(crime.date).toString()
 
             binding.root.setOnClickListener {
                 Toast.makeText(
@@ -24,18 +26,28 @@ class CrimeHolder(
                     Toast.LENGTH_SHORT
                 ).show()
             }
-        }else if(binding is ListItemSeriousCrimeBinding){
-            binding.crimeTitle.text = crime.title
-            binding.crimeDate.text = crime.date.toString()
-
-            binding.root.setOnClickListener {
-                Toast.makeText(
-                    binding.root.context,
-                    "${crime.title} clicked: Serious Crime",
-                    Toast.LENGTH_SHORT
-                ).show()
+            binding.crimeSolved.visibility = if (crime.requiresPolice){
+                View.VISIBLE
+            }else{
+                View.GONE
             }
-        }
+//        }else if(binding is ListItemSeriousCrimeBinding){
+//            binding.crimeTitle.text = crime.title
+//            binding.crimeDate.text = crime.date.toString()
+//
+//            binding.root.setOnClickListener {
+//                Toast.makeText(
+//                    binding.root.context,
+//                    "${crime.title} clicked: Serious Crime",
+//                    Toast.LENGTH_SHORT
+//                ).show()
+//            }
+//            binding.crimeSolved.visibility = if (crime.requiresPolice){
+//                View.VISIBLE
+//            }else{
+//                View.GONE
+//            }
+//        }
     }
 }
 
@@ -43,11 +55,11 @@ class CrimeListAdapter(private val crime: List<Crime>): RecyclerView.Adapter<Cri
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeHolder {
 
         val inflater = LayoutInflater.from(parent.context)
-        val binding = if (viewType == VIEW_TYPE_NORMAL_CRIME) {
+        val binding = // if (viewType == VIEW_TYPE_NORMAL_CRIME) {
             ListItemCrimeBinding.inflate(inflater, parent, false)
-        }else{
-            ListItemSeriousCrimeBinding.inflate(inflater, parent, false)
-        }
+//        }else{
+//            ListItemSeriousCrimeBinding.inflate(inflater, parent, false)
+//        }
         return CrimeHolder(binding)
     }
 
@@ -58,14 +70,14 @@ class CrimeListAdapter(private val crime: List<Crime>): RecyclerView.Adapter<Cri
         holder.bind(crime)
     }
 
-    override fun getItemViewType(position: Int): Int {
-        val crime = crime[position]
-        return if (crime.requiresPolice){
-            VIEW_TYPE_SERIOUS_CRIME
-        }else{
-            VIEW_TYPE_NORMAL_CRIME
-        }
-    }
+//    override fun getItemViewType(position: Int): Int {
+//        val crime = crime[position]
+////        return if (crime.requiresPolice){
+////            VIEW_TYPE_SERIOUS_CRIME
+////        }else{
+////            VIEW_TYPE_NORMAL_CRIME
+////        }
+//    }
 
     companion object{
         private const val VIEW_TYPE_NORMAL_CRIME = 0
